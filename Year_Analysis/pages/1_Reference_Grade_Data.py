e97991721952cf1f8296e7931936ddac5e6344a8
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-25 12:30:21 +0000

    Update 1_Reference_Grade_Data.py

--- Year_Analysis/pages/1_Reference_Grade_Data.py
+++ Year_Analysis/pages/1_Reference_Grade_Data.py
@@ -406,11 +406,11 @@ def calculate_exceedances(df):
     return exceedance
 
 def calculate_min_max(df):
-    daily_avg = df.groupby(['site', 'day', 'year', 'month'], as_index=False).agg({
+    daily_avg = df.groupby(['site', 'day', 'year'], as_index=False).agg({
         'pm25': 'mean',
         'pm10': 'mean'
     })
-    df_min_max = daily_avg.groupby(['year', 'site', 'month'], as_index=False).agg(
+    df_min_max = daily_avg.groupby(['year', 'site'], as_index=False).agg(
         daily_avg_pm10_max=('pm10', lambda x: round(x.max(), 1)),
         daily_avg_pm10_min=('pm10', lambda x: round(x.min(), 1)),
         daily_avg_pm25_max=('pm25', lambda x: round(x.max(), 1)),
